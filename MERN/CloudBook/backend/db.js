6f0e5ee6625ef232804159014ad369f3a65bb819
Sachin Saurav <ersachinsaurav@gmail.com>
2023-02-20 22:56:31 +0530

    fixed minor bug in cloudBook

--- MERN/CloudBook/backend/db.js
+++ MERN/CloudBook/backend/db.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017/cloudbook";
+const mongoose = require('mongoose');
+const mongoURI = 'mongodb://127.0.0.1:27017/cloudbook';
 
 const connectToMongo = () => {
-  mongoose.connect(mongoURI, () => {});
+	mongoose.connect(mongoURI, () => {});
 };
 
 module.exports = connectToMongo;
