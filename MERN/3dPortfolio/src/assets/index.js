645ba5b327baaa386e068db529edce5dfd01a7d3
Sachin Saurav <ersachinsaurav@gmail.com>
2023-04-16 21:43:19 +0530

    moved 3dPortfolio to MERN

---
+++ MERN/3dPortfolio/src/assets/index.js
@@ -0,0 +1,84 @@
+import logo from './logo.png';
+import backend from './backend.png';
+import creator from './creator.png';
+import web from './web.png';
+import pern from './pern.png';
+import mern from './mern.png';
+import lamp from './lamp.png';
+import github from './github.png';
+import menu from './menu.svg';
+import close from './close.svg';
+
+import html from './tech/html.png';
+import css from './tech/css.png';
+import javascript from './tech/javascript.png';
+import php from './tech/php.png';
+import nodejs from './tech/nodejs.png';
+import reactjs from './tech/reactjs.png';
+import nextjs from './tech/nextjs.png';
+import redux from './tech/redux.png';
+import typescript from './tech/typescript.png';
+import expressjs from './tech/expressjs.png';
+import postgres from './tech/postgres.png';
+import mysql from './tech/mysql.png';
+import git from './tech/git.png';
+
+import savisoft from './company/savisoft.png';
+import entrata from './company/entrata.png';
+
+import sauraviCafe from './projects/sauraviCafe.png';
+import blogera from './projects/blogera.png';
+import cloudBook from './projects/cloudBook.png';
+import newsBilla from './projects/newsBilla.png';
+import nodeChat from './projects/nodeChat.png';
+import weatherBot from './projects/weatherBot.png';
+
+import sachin from './testimonials/sachin.png';
+import raj from './testimonials/raj.png';
+import chris from './testimonials/chris.png';
+
+import computerCanvasLite from './litePNGs/computerCanvasLite.png';
+import earthCanvasLite from './litePNGs/earthCanvasLite.png';
+import starsCanvasLite from './litePNGs/starsCanvasLite.jpg';
+import techCanvasLite from './litePNGs/techCanvasLite.png';
+
+export {
+	sauraviCafe,
+	blogera,
+	cloudBook,
+	newsBilla,
+	nodeChat,
+	weatherBot,
+	sachin,
+	raj,
+	chris,
+	php,
+	nextjs,
+	expressjs,
+	postgres,
+	mysql,
+	lamp,
+	pern,
+	mern,
+	logo,
+	backend,
+	creator,
+	web,
+	github,
+	menu,
+	close,
+	css,
+	git,
+	html,
+	javascript,
+	nodejs,
+	reactjs,
+	redux,
+	typescript,
+	savisoft,
+	entrata,
+	computerCanvasLite,
+	earthCanvasLite,
+	starsCanvasLite,
+	techCanvasLite,
+};
