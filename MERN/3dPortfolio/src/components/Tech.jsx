1c9de0c281ad20e5311e5df9a7bcba5d6586ca7d
Sachin Saurav <ersachinsaurav@gmail.com>
2023-04-16 21:43:19 +0530

    moved 3dPortfolio to MERN

---
+++ MERN/3dPortfolio/src/components/Tech.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { BallCanvas } from './canvas';
+import { SectionWrapper } from '../hoc';
+import { technologies } from '../constants';
+import { isMobile } from 'react-device-detect';
+import { techCanvasLite } from '../assets';
+
+const Tech = () => {
+	return (
+		<div className="flex flex-row flex-wrap justify-center gap-10">
+			{isMobile && <img src={techCanvasLite} className="w-full h-auto" />}
+			{!isMobile &&
+				technologies.map((technology) => (
+					<div className="w-28 h-28" key={technology.name}>
+						<BallCanvas icon={technology.icon} />
+					</div>
+				))}
+		</div>
+	);
+};
+
+export default SectionWrapper(Tech, '');
