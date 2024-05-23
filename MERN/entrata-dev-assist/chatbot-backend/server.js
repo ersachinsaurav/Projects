6bfa607c9b5852813768a24929167d9148030c86
Sachin Saurav <ssaurav@entrata.com>
2024-05-23 18:01:47 +0530

    Completed DevTa
    Entrata Dev Assist

---
+++ MERN/entrata-dev-assist/chatbot-backend/server.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { OpenAI } = require('openai');
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const app = express();
+const port = 3001;
+
+const openai = new OpenAI();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Load CSV Data
+const loadCSVData = () => {
+  return new Promise((resolve, reject) => {
+    const results = [];
+    fs.createReadStream('trainingData.csv')
+      .pipe(csv())
+      .on('data', (data) => results.push(data))
+      .on('end', () => resolve(results))
+      .on('error', (error) => reject(error));
+  });
+};
+
+let data;
+loadCSVData().then((csvData) => {
+  data = csvData;
+});
+
+app.post('/ask', async (req, res) => {
+  const { question, history } = req.body;
+
+  //Not maintainign history for now
+
+  const prompt = `You are given the following CSV data:\n\n${JSON.stringify(
+    data,
+    null,
+    2
+  )}
+  \n\nAnswer the following question based on this data: ${question}
+  \n\nFollow these instructions while answering the question:
+  \n\nPlease adhere to the following guidelines when responding to the question:
+  1. Provide the answer directly without referencing the source.
+  2. If the CSV does not contain the answer, respond with your own answer.
+  3. If the question is meant to conclude the chat, simply express thanks and end the conversation.`;
+
+  try {
+    const response = await openai.chat.completions.create({
+      model: 'gpt-4',
+      messages: [
+        {
+          role: 'user',
+          content: prompt,
+        },
+      ],
+    });
+
+    const answer = response.choices[0].message.content;
+    res.json({ answer });
+  } catch (error) {
+    console.error('Error asking question:', error);
+    res.status(500).json({ error: 'Error generating response from OpenAI' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ MERN/entrata-dev-assist/chatbot-frontend/src/App.js
@@ -0,0 +1,290 @@
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+function App() {
+  const [question, setQuestion] = useState('');
+  const [chatHistory, setChatHistory] = useState([
+    {
+      role: 'assistant',
+      content:
+        'Hello, I’m Entrata Dev Assist aka DevTa. What kind of support are you looking for?',
+    },
+  ]);
+  const [isChatVisible, setIsChatVisible] = useState(false);
+  const [stage, setStage] = useState(0);
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [showOptions, setShowOptions] = useState(true);
+  const [showQuitOption, setShowQuitOption] = useState(false);
+  const [isBotTyping, setIsBotTyping] = useState(false);
+  const [showInput, setShowInput] = useState(false);
+  const chatContainerRef = useRef(null);
+
+  const initialOptions = [
+    { label: 'Docker', value: 'Docker' },
+    { label: 'PHPStorm', value: 'PHPStorm' },
+    { label: 'GIT', value: 'GIT' },
+    { label: 'Other', value: 'Other' },
+  ];
+
+  const subOptions = {
+    Docker: [
+      { label: 'Docker Setup', value: 'Docker Setup' },
+      { label: 'Docker Commands', value: 'Docker Commands' },
+      { label: 'Issues', value: 'Issues' },
+    ],
+    PHPStorm: [
+      { label: 'PHPStorm Setup', value: 'PHPStorm Setup' },
+      { label: 'PHPUnit Setup', value: 'PHPUnit Setup' },
+      { label: 'CodeSniffer', value: 'CodeSniffer' },
+      { label: 'Issues', value: 'Issues' },
+    ],
+    GIT: [
+      { label: 'Git Setup', value: 'Git Setup' },
+      { label: 'GIT Commands', value: 'GIT Commands' },
+      { label: 'Issues', value: 'Issues' },
+    ],
+  };
+
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop =
+        chatContainerRef.current.scrollHeight;
+    }
+  }, [chatHistory, isBotTyping]);
+
+  const handleOptionSelect = (option) => {
+    setChatHistory((prevChatHistory) => [
+      ...prevChatHistory,
+      { role: 'user', content: option.label },
+    ]);
+
+    if (option.value === 'quit') {
+      setChatHistory((prevChatHistory) => [
+        ...prevChatHistory,
+        {
+          role: 'assistant',
+          content: 'Thank you for using EntrataDevAssist. Have a great day!',
+        },
+      ]);
+      setShowOptions(false);
+      setShowQuitOption(false);
+      setTimeout(() => {
+        setIsChatVisible(false);
+        resetChat();
+      }, 2000);
+      return;
+    }
+
+    if (stage === 0) {
+      setSelectedCategory(option.value);
+      setShowInput(false);
+      if (option.value === 'Other') {
+        setChatHistory((prevChatHistory) => [
+          ...prevChatHistory,
+          { role: 'assistant', content: 'Please type your question.' },
+        ]);
+        setShowQuitOption(true);
+        setShowInput(true);
+      } else {
+        setChatHistory((prevChatHistory) => [
+          ...prevChatHistory,
+          {
+            role: 'assistant',
+            content: `In ${option.label}, how can I help you with?`,
+          },
+        ]);
+      }
+      setStage(1);
+      setShowOptions(true);
+    } else if (stage === 1) {
+      if (option.value === 'Issues') {
+        setChatHistory((prevChatHistory) => [
+          ...prevChatHistory,
+          {
+            role: 'assistant',
+            content: 'Please type your question regarding issues.',
+          },
+        ]);
+        setShowOptions(false);
+        setShowQuitOption(true);
+        setShowInput(true);
+      } else {
+        askQuestion(option.value);
+        setShowOptions(false);
+        setStage(0);
+        setSelectedCategory('');
+        setShowQuitOption(true);
+        setShowInput(false);
+      }
+    }
+  };
+
+  const askQuestion = async (userQuestion) => {
+    const newChatHistory = [
+      ...chatHistory,
+      { role: 'user', content: userQuestion },
+    ];
+    setChatHistory(newChatHistory);
+    setQuestion('');
+    setIsBotTyping(true);
+
+    try {
+      const response = await axios.post('http://localhost:3001/ask', {
+        question: userQuestion,
+        history: newChatHistory.map((message) => ({
+          role: message.role,
+          content: message.content,
+        })),
+      });
+
+      const answer = response.data.answer;
+      setIsBotTyping(false);
+      setChatHistory((prevChatHistory) => [
+        ...prevChatHistory,
+        { role: 'assistant', content: answer },
+        { role: 'assistant', content: 'Anything else I can help you with?' },
+      ]);
+      setShowOptions(true);
+      setStage(0);
+      setShowInput(false);
+      setShowQuitOption(true); // Show Quit option after one interaction cycle
+    } catch (error) {
+      setIsBotTyping(false);
+      setChatHistory((prevChatHistory) => [
+        ...prevChatHistory,
+        { role: 'assistant', content: 'An error occurred.' },
+      ]);
+      setShowOptions(true);
+      setStage(0);
+      setShowInput(false);
+      setShowQuitOption(true); // Show Quit option even if an error occurs
+    }
+  };
+
+  const createMarkup = (html) => {
+    const urlRegex = /(https?:\/\/[^\s]+)/g;
+    const htmlWithLinks = html.replace(urlRegex, (url) => {
+      return `<a href="${url}" target="_blank" rel="noopener noreferrer">${url}</a>`;
+    });
+    return { __html: htmlWithLinks.replace(/\n/g, '<br>') };
+  };
+
+  const renderOptions = (options, includeQuit) => (
+    <div className="options">
+      {options.map((option, index) => (
+        <button key={index} onClick={() => handleOptionSelect(option)}>
+          {option.label}
+        </button>
+      ))}
+      {includeQuit && (
+        <button
+          onClick={() => handleOptionSelect({ label: 'Quit', value: 'quit' })}
+        >
+          Quit
+        </button>
+      )}
+    </div>
+  );
+
+  const resetChat = () => {
+    setQuestion('');
+    setChatHistory([
+      {
+        role: 'assistant',
+        content:
+          'Hello, I’m EntrataDevAssist. What kind of support are you looking for?',
+      },
+    ]);
+    setStage(0);
+    setSelectedCategory('');
+    setShowOptions(true);
+    setShowQuitOption(false);
+    setShowInput(false);
+  };
+
+  return (
+    <div className="App">
+      <button
+        className="toggle-chat-btn"
+        onClick={() => setIsChatVisible(!isChatVisible)}
+      >
+        {isChatVisible ? 'Hide DevTa' : 'Launch DevTa'}
+      </button>
+      {isChatVisible && (
+        <div className="chat-window">
+          <div className="chat-header">
+            <i className="fas fa-robot"></i>
+            <h1>DevTa</h1>
+          </div>
+          <div className="chat-container" ref={chatContainerRef}>
+            {chatHistory.map((message, index) => (
+              <div key={index} className={`chat-message ${message.role}`}>
+                <div className="chat-avatar">
+                  <i
+                    className={`fas ${
+                      message.role === 'user' ? 'fa-user' : 'fa-robot'
+                    }`}
+                  ></i>
+                </div>
+                <div
+                  className="chat-bubble"
+                  dangerouslySetInnerHTML={createMarkup(message.content)}
+                ></div>
+              </div>
+            ))}
+            {isBotTyping && (
+              <div className="chat-message assistant">
+                <div className="chat-avatar">
+                  <i className="fas fa-robot"></i>
+                </div>
+                <div className="chat-bubble">
+                  <i>Bot is typing...</i>
+                </div>
+              </div>
+            )}
+            {showOptions && (
+              <>
+                {stage === 0 && renderOptions(initialOptions, false)}
+                {stage === 1 &&
+                  selectedCategory !== 'Other' &&
+                  !showInput &&
+                  renderOptions(subOptions[selectedCategory], false)}
+              </>
+            )}
+            {showInput && (
+              <div className="input-container">
+                <input
+                  type="text"
+                  value={question}
+                  onChange={(e) => setQuestion(e.target.value)}
+                  placeholder={`Type your question regarding ${selectedCategory}`}
+                  onKeyPress={(e) => {
+                    if (e.key === 'Enter') askQuestion(question);
+                  }}
+                />
+                <button onClick={() => askQuestion(question)}>
+                  <i className="fas fa-paper-plane"></i>
+                </button>
+              </div>
+            )}
+            {showQuitOption && stage !== 1 && (
+              <div className="options">
+                <button
+                  onClick={() =>
+                    handleOptionSelect({ label: 'Quit', value: 'quit' })
+                  }
+                >
+                  Quit
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
